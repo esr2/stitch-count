3e246cf5028fffe8fdeb156b980d83f5ac81ad76
Erin <erin.s.rosenbaum@gmail.com>
2020-07-16 18:46:40 -0700

    load pdf from server

--- init.ts
+++ init.ts
@@ -17,6 +17,8 @@ function onLoad() {
           updateDisplay(project);
         });
   });
+
+  loadPdf();
 }
 
 function updateDisplay(project: Project) {
@@ -55,5 +57,51 @@ function createProject() : Promise<Project> {
     };
     oXHR.open("GET", "https://esr2.github.io/stitch-count/celtic_throw.json", true);  // get json file.
     oXHR.send();
+  });
+}
+
+function loadPdf() {
+  // If absolute URL from the remote server is provided, configure the CORS
+  // header on that server.
+  var url = 'https://github.com/esr2/stitch-count/blob/master/AK-Celtic_Traveller_Throw-v052220.pdf';
+
+  // Loaded via <script> tag, create shortcut to access PDF.js exports.
+  var pdfjsLib = window['pdfjs-dist/build/pdf'];
+
+  // The workerSrc property shall be specified.
+  pdfjsLib.GlobalWorkerOptions.workerSrc = 'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.5.207/pdf.worker.min.js';
+
+  // Asynchronous download of PDF
+  var loadingTask = pdfjsLib.getDocument(url);
+  loadingTask.promise.then(function(pdf) {
+    console.log('PDF loaded');
+
+    // Fetch the first page
+    var pageNumber = 1;
+    pdf.getPage(pageNumber).then(function(page) {
+      console.log('Page loaded');
+
+      var scale = 1.5;
+      var viewport = page.getViewport({scale: scale});
+
+      // Prepare canvas using PDF page dimensions
+      var canvas = document.getElementById('the-canvas') as HTMLCanvasElement;
+      var context = canvas.getContext('2d');
+      canvas.height = viewport.height;
+      canvas.width = viewport.width;
+
+      // Render PDF page into canvas context
+      var renderContext = {
+        canvasContext: context,
+        viewport: viewport
+      };
+      var renderTask = page.render(renderContext);
+      renderTask.promise.then(function () {
+        console.log('Page rendered');
+      });
+    });
+  }, function (reason) {
+    // PDF loading error
+    console.error(reason);
   });
 }
