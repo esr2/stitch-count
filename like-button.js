d00eebaf094eacb97cace3e2d470ac550566ed5e
Erin Rosenbaum <erin.s.rosenbaum@gmail.com>
2023-03-15 21:40:50 -0700

    Prettier

--- like-button.js
+++ like-button.js
@@ -1,12 +1,12 @@
-'use strict';
+"use strict";
 function LikeButton() {
     const [liked, setLiked] = React.useState(false);
     if (liked) {
-        return 'You liked this!';
+        return "You liked this!";
     }
-    return (React.createElement("button", { onClick: () => setLiked(true) }, "Like"));
+    return React.createElement("button", { onClick: () => setLiked(true) }, "Like");
 }
-const rootNode = document.getElementById('like-button-root');
+const rootNode = document.getElementById("like-button-root");
 const root = ReactDOM.createRoot(rootNode);
 root.render(React.createElement(LikeButton));
 //# sourceMappingURL=like-button.js.map
