cc6c7f5ce1c6b1ff324487ad89367bad490cf399
Erin Rosenbaum <erin.s.rosenbaum@gmail.com>
2023-03-17 15:51:17 -0700

    Wrap project picker in a Card

--- src/ProjectPicker.tsx
+++ src/ProjectPicker.tsx
@@ -1,29 +1,35 @@
-import { Button } from "reactstrap";
+import { Card, CardBody, CardHeader, Button } from "reactstrap";
 
 import { ProjectDetails, PROJECT_VALUES } from "./ProjectDetails";
 
 function ProjectPicker() {
   return (
-    <>
-      {PROJECT_VALUES.map((value: ProjectDetails) => {
-        return (
-          <div className="custom-control custom-radio mb-3">
-            <input
-              className="custom-control-input"
-              id={value.storageKey}
-              defaultChecked={value.isDefault}
-              name="custom-radio-2"
-              type="radio"
-            />
-            <label className="custom-control-label" htmlFor={value.storageKey}>
-              {value.name}
-            </label>
-          </div>
-        );
-      })}
+    <Card className="shadow">
+      <CardHeader>Projects</CardHeader>
+      <CardBody>
+        {PROJECT_VALUES.map((value: ProjectDetails) => {
+          return (
+            <div className="custom-control custom-radio mb-3">
+              <input
+                className="custom-control-input"
+                id={value.storageKey}
+                defaultChecked={value.isDefault}
+                name="custom-radio-2"
+                type="radio"
+              />
+              <label
+                className="custom-control-label"
+                htmlFor={value.storageKey}
+              >
+                {value.name}
+              </label>
+            </div>
+          );
+        })}
 
-      <Button color="primary">Pick</Button>
-    </>
+        <Button color="primary">Pick</Button>
+      </CardBody>
+    </Card>
   );
 }
 
