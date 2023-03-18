8d6748b30bfdff120c6fd041970f0c976b14c234
Erin Rosenbaum <erin.s.rosenbaum@gmail.com>
2023-03-17 21:30:25 -0700

    First attempt at cargo culting background. Weird bottom section

--- src/App.tsx
+++ src/App.tsx
@@ -13,18 +13,44 @@ function App() {
 
   return (
     <>
-      <Navbar className="navbar-horizontal navbar-dark bg-default" expand="lg">
+      <Navbar
+        className="navbar-main navbar-transparent navbar-light bg-default"
+        expand="lg"
+      >
         <Container>
           <NavbarBrand href="#" onClick={(e) => e.preventDefault()}>
             {!selectedProjectId ? "Stitch Count" : selectedProjectId}
           </NavbarBrand>
         </Container>
       </Navbar>
-      {!selectedProjectId ? (
-        <ProjectPicker onProjectPick={onProjectPick} />
-      ) : (
-        <Project projectId={selectedProjectId} />
-      )}
+      <main>
+        <div className="position-relative">
+          {/* shape Hero */}
+          <section className="section section-lg section-shaped pb-250">
+            <div className="shape shape-style-1 shape-default">
+              <span />
+              <span />
+              <span />
+              <span />
+              <span />
+              <span />
+              <span />
+              <span />
+              <span />
+            </div>
+            <Container>
+              {!selectedProjectId ? (
+                <ProjectPicker onProjectPick={onProjectPick} />
+              ) : (
+                <Project projectId={selectedProjectId} />
+              )}
+            </Container>
+          </section>
+          <section className="section section-lg bg-gradient-blue">
+            <Container className="pt-lg pb-300"></Container>
+          </section>
+        </div>
+      </main>
     </>
   );
 }
